ec97dd1a9dbe954a42ef177e02ac43c886afcc66
KamielFlamiel <justinmeijer2007@outlook.com>
2024-09-13 15:22:43 +0200

    Project toegevoegd

---
+++ Mastermind-Kamiel/Mastermind.java
@@ -0,0 +1,61 @@
+package mastermind;
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		
+	boolean hideBox = true;
+	
+	// B= Breaker van Codebreaker.
+	// M= Maker van Codemaker.
+	
+	int row1B, row2B, row3B, row4B, row5B, row6B, row7B, row8B, row9B, row10B, row11M;
+    row1B = 1;
+    row2B = 2;
+    row3B = 3;
+    row4B = 4; 
+    row5B = 5;
+    row6B = 6;
+    row7B = 7;
+    row8B = 8;
+    row9B = 9;
+    row10B = 10;
+    row11M = 11;
+	
+	double space1B, space2B, space3B, space4B, space5M, space6M, space7M, space8M; 
+    space1B = 1.1;
+    space2B = 1.2;
+    space3B = 1.3;
+    space4B = 1.4;
+    
+    space5M = 1.5;
+    space6M = 1.6;
+    space7M = 1.7;
+    space8M = 1.8;
+    
+    String red = "red";
+    String blue = "blue";
+    String purple = "purple";
+    String yellow = "yellow";
+    String green = "green";
+    String orange = "orange";
+    
+    
+    // 11  1.1, 1.2, 1.3, 1.4
+    //
+    // 10  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  9  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  8  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  7  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  6  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  5  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  4  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  3  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  2  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    //  1  1.1, 1.2, 1.3, 1.4,    1.5, 1.6, 1.7, 1.8
+    
+	
+
+	}
+
+}
---
+++ Mastermind-Kamiel/src/mastermind/Mastermind.java
@@ -0,0 +1,25 @@
+package mastermind;
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		
+        boolean hideBox = true;
+		
+		int red = 1;
+		int blue = 2;
+		int yellow = 3;
+		int purple = 4;
+		int green = 5;
+		int black = 6;
+		int white = 7;
+		
+		int breakerRow1Space1;
+	
+		breakerRow1Space1 = red;
+		
+		
+
+	}
+
+}
