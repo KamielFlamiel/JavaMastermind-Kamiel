7f7a9598779335d700e63da394491828b42ac493
KamielFlamiel <justinmeijer2007@outlook.com>
2024-09-16 14:11:53 +0200

    Input opdracht gemaakt (wauw)
    input is (wauw)

---
+++ UserInput/src/input03/Input03.java
@@ -0,0 +1,37 @@
+package input03;
+
+import java.util.Scanner;
+
+class Input03 {
+
+    public static void main(String[] args) {
+        //Create a Scanner
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int x = sc.nextInt();
+    	int y = sc.nextInt();
+    	int z = sc.nextInt();
+    	
+    	System.out.println(x);
+    	System.out.println(y);
+    	System.out.println(z);
+        //Find and print the sum of three integers entered by the user
+    	
+
+    	
+        
+    	 
+       
+        
+       
+       
+       
+       
+     
+        
+        sc.close();
+        //Remember to close the Scanner
+        
+    }
+
+}
