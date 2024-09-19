b2e9c20520ccd0bed72d1732a5da34a788ab1cfd
KamielFlamiel <justinmeijer2007@outlook.com>
2024-09-19 15:16:55 +0200

    NAAR HUIS

--- Mastermind-Kamiel/src/mastermind1/Mastermind1.java
+++ Mastermind-Kamiel/src/mastermind1/Mastermind1.java
@@ -121,6 +121,11 @@ public class Mastermind1 {
 		int breakerRow2Space2;
 		int breakerRow2Space3;
 		int breakerRow2Space4;
+		
+		breakerRow2Space1 = sc.nextInt();
+		breakerRow2Space2 = sc.nextInt();
+		breakerRow2Space3 = sc.nextInt();
+		breakerRow2Space4 = sc.nextInt();
 
 		int makerRow2Space1;
 		int makerRow2Space2;
@@ -131,6 +136,11 @@ public class Mastermind1 {
 		int breakerRow3Space2;
 		int breakerRow3Space3;
 		int breakerRow3Space4;
+		
+		breakerRow3Space1 = sc.nextInt();
+		breakerRow3Space2 = sc.nextInt();
+		breakerRow3Space3 = sc.nextInt();
+		breakerRow3Space4 = sc.nextInt();
 
 		int makerRow3Space1;
 		int makerRow3Space2;
@@ -141,6 +151,11 @@ public class Mastermind1 {
 		int breakerRow4Space2;
 		int breakerRow4Space3;
 		int breakerRow4Space4;
+		
+		breakerRow4Space1 = sc.nextInt();
+		breakerRow4Space2 = sc.nextInt();
+		breakerRow4Space3 = sc.nextInt();
+		breakerRow4Space4 = sc.nextInt();
 
 		int makerRow4Space1;
 		int makerRow4Space2;
@@ -151,6 +166,11 @@ public class Mastermind1 {
 		int breakerRow5Space2;
 		int breakerRow5Space3;
 		int breakerRow5Space4;
+		
+		breakerRow5Space1 = sc.nextInt();
+		breakerRow5Space2 = sc.nextInt();
+		breakerRow5Space3 = sc.nextInt();
+		breakerRow5Space4 = sc.nextInt();
 
 		int makerRow5Space1;
 		int makerRow5Space2;
@@ -161,6 +181,21 @@ public class Mastermind1 {
 		int breakerRow6Space2;
 		int breakerRow6Space3;
 		int breakerRow6Space4;
+		
+		breakerRow6Space1 = sc.nextInt();
+		breakerRow6Space2 = sc.nextInt();
+		breakerRow6Space3 = sc.nextInt();
+		breakerRow6Space4 = sc.nextInt();
+		
+		int breakerRow7Space1;
+		int breakerRow7Space2;
+		int breakerRow7Space3;
+		int breakerRow7Space4;
+		
+		breakerRow7Space1 = sc.nextInt();
+		breakerRow7Space2 = sc.nextInt();
+		breakerRow7Space3 = sc.nextInt();
+		breakerRow7Space4 = sc.nextInt();
 
 		int makerRow7Space1;
 		int makerRow7Space2;
@@ -171,6 +206,11 @@ public class Mastermind1 {
 		int breakerRow8Space2;
 		int breakerRow8Space3;
 		int breakerRow8Space4;
+		
+		breakerRow8Space1 = sc.nextInt();
+		breakerRow8Space2 = sc.nextInt();
+		breakerRow8Space3 = sc.nextInt();
+		breakerRow8Space4 = sc.nextInt();
 
 		int makerRow8Space1;
 		int makerRow8Space2;
@@ -181,6 +221,11 @@ public class Mastermind1 {
 		int breakerRow9Space2;
 		int breakerRow9Space3;
 		int breakerRow9Space4;
+		
+		breakerRow9Space1 = sc.nextInt();
+		breakerRow9Space2 = sc.nextInt();
+		breakerRow9Space3 = sc.nextInt();
+		breakerRow9Space4 = sc.nextInt();
 
 		int makerRow9Space1;
 		int makerRow9Space2;
@@ -191,21 +236,18 @@ public class Mastermind1 {
 		int breakerRow10Space2;
 		int breakerRow10Space3;
 		int breakerRow10Space4;
+		
+		breakerRow10Space1 = sc.nextInt();
+		breakerRow10Space2 = sc.nextInt();
+		breakerRow10Space3 = sc.nextInt();
+		breakerRow10Space4 = sc.nextInt();
 
 		int makerRow10Space1;
 		int makerRow10Space2;
 		int makerRow10Space3;
 		int makerRow10Space4;
 
-		breakerRow1Space1 = red;
-		breakerRow1Space2 = red;
-		breakerRow1Space3 = red;
-		breakerRow1Space4 = red;
-
-		makerRow1Space1 = white;
-		makerRow1Space2 = white;
-		makerRow1Space3 = white;
-		makerRow1Space4 = white;
+		
 
 	}
 
