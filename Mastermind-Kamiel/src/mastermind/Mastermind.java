93fdf3affce86499c74b197c12ec307359a26ed7
KamielFlamiel <justinmeijer2007@outlook.com>
2024-10-07 14:30:32 +0200

    uphie

--- Mastermind-Kamiel/src/mastermind/Mastermind.java
+++ Mastermind-Kamiel/src/mastermind/Mastermind.java
@@ -4,59 +4,59 @@ public class Mastermind {
 
 	public static void main(String[] args) {
 		
-        boolean hideBox = true;
-        
-        double space1B_row1, space2B_row1, space3B_row1, space4B_row1, space5M_row1, space6M_row1, space7M_row1, space8M_row1;
-		double space1B_row2, space2B_row2, space3B_row2, space4B_row2, space5M_row2, space6M_row2, space7M_row2, space8M_row2;
-		double space1B_row3, space2B_row3, space3B_row3, space4B_row3, space5M_row3, space6M_row3, space7M_row3, space8M_row3;
-		
-	    space1B_row1 = 1.1;     
-	    space2B_row1 = 1.2;
-	    space3B_row1 = 1.3;
-	    space4B_row1 = 1.4;
-	    
-	    space5M_row1 = 1.5;
-	    space6M_row1 = 1.6;
-	    space7M_row1 = 1.7;
-	    space8M_row1 = 1.8;
-	    
-	    space1B_row2 = 2.1;
-	    space2B_row2 = 2.2;
-	    space3B_row2 = 2.3;
-	    space4B_row2 = 2.4;
-	    
-	    space5M_row2 = 2.5;
-	    space6M_row2 = 2.6;
-	    space7M_row2 = 2.7;
-	    space8M_row2 = 2.8;
-	    
-	    space1B_row3 = 3.1;
-	    space2B_row3 = 3.2;
-	    space3B_row3 = 3.3;
-	    space4B_row3 = 3.4;
-	    
-	    space5M_row3 = 3.5;
-	    space6M_row3 = 3.6;
-	    space7M_row3 = 3.7;
-	    space8M_row3 = 3.8;
-	    
-        
-        
-		int red = 1;
-		int blue = 2;
-		int yellow = 3;
-		int purple = 4;
-		int green = 5;
-		int orange = 6;
-		int black = 7;
-		int white = 8;
-		
-		int breakerRow1Space1;
-	
-		breakerRow1Space1 = red;
-		breakerRow1Space2
-		
-		System.out.println("");
+//        boolean hideBox = true;
+//        
+//        double space1B_row1, space2B_row1, space3B_row1, space4B_row1, space5M_row1, space6M_row1, space7M_row1, space8M_row1;
+//		double space1B_row2, space2B_row2, space3B_row2, space4B_row2, space5M_row2, space6M_row2, space7M_row2, space8M_row2;
+//		double space1B_row3, space2B_row3, space3B_row3, space4B_row3, space5M_row3, space6M_row3, space7M_row3, space8M_row3;
+//		
+//	    space1B_row1 = 1.1;     
+//	    space2B_row1 = 1.2;
+//	    space3B_row1 = 1.3;
+//	    space4B_row1 = 1.4;
+//	    
+//	    space5M_row1 = 1.5;
+//	    space6M_row1 = 1.6;
+//	    space7M_row1 = 1.7;
+//	    space8M_row1 = 1.8;
+//	    
+//	    space1B_row2 = 2.1;
+//	    space2B_row2 = 2.2;
+//	    space3B_row2 = 2.3;
+//	    space4B_row2 = 2.4;
+//	    
+//	    space5M_row2 = 2.5;
+//	    space6M_row2 = 2.6;
+//	    space7M_row2 = 2.7;
+//	    space8M_row2 = 2.8;
+//	    
+//	    space1B_row3 = 3.1;
+//	    space2B_row3 = 3.2;
+//	    space3B_row3 = 3.3;
+//	    space4B_row3 = 3.4;
+//	    
+//	    space5M_row3 = 3.5;
+//	    space6M_row3 = 3.6;
+//	    space7M_row3 = 3.7;
+//	    space8M_row3 = 3.8;
+//	    
+//        
+//        
+//		int red = 1;
+//		int blue = 2;
+//		int yellow = 3;
+//		int purple = 4;
+//		int green = 5;
+//		int orange = 6;
+//		int black = 7;
+//		int white = 8;
+//		
+//		int breakerRow1Space1;
+//	
+//		breakerRow1Space1 = red;
+//		breakerRow1Space2
+//		
+//		System.out.println("");
 		
 		
 
--- Mastermind-Kamiel/src/mastermind1/Flowchart_mastermind.java
+++ Mastermind-Kamiel/src/mastermind1/Flowchart_mastermind.java
@@ -1,170 +1,231 @@
- package mastermind1;
- import java.util.Scanner;
+package mastermind1;
+
+import java.util.Scanner;
 
 public class Flowchart_mastermind {
-    private static Scanner input = new Scanner(System.in);
+	private static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int black = 7;
-        int white = 8;
- 
-        System.out.println("Maker, Create combination.");
-        System.out.println("Red = 1");
-        System.out.println("Blue = 2");
-        System.out.println("Yellow = 3");
-        System.out.println("Purple = 4");
-        System.out.println("Green = 5");
-        System.out.println("Orange = 6");
-        int makerCombinationAnswer1;
-        int makerCombinationAnswer2;
-        int makerCombinationAnswer3;
-        int makerCombinationAnswer4;
-        int verify;
+	public static void main(String[] args) {
+		int black = 7;
+		int white = 8;
+		int empty = 0;
+		
+		System.out.println("Maker, Create combination.");
+		System.out.println("Red = 1");
+		System.out.println("Blue = 2");
+		System.out.println("Yellow = 3");
+		System.out.println("Purple = 4");
+		System.out.println("Green = 5");
+		System.out.println("Orange = 6");
+		int[] makerCombinationAnswer = new int[4];
+		int verify;
 
-        do {
-            System.out.println("Create combination");
-            makerCombinationAnswer1 = input.nextInt();
-            if (makerCombinationAnswer1 >= 6) {
-                makerCombinationAnswer1 = 6;
-            } else {
-                if (makerCombinationAnswer1 <= 1) {
-                    makerCombinationAnswer1 = 1;
-                }
-            }
-            makerCombinationAnswer2 = input.nextInt();
-            if (makerCombinationAnswer2 >= 6) {
-                makerCombinationAnswer2 = 6;
-            } else {
-                if (makerCombinationAnswer2 <= 1) {
-                    makerCombinationAnswer2 = 1;
-                }
-            }
-            makerCombinationAnswer3 = input.nextInt();
-            if (makerCombinationAnswer3 >= 6) {
-                makerCombinationAnswer3 = 6;
-            } else {
-                if (makerCombinationAnswer3 <= 1) {
-                    makerCombinationAnswer3 = 1;
-                }
-            }
-            makerCombinationAnswer4 = input.nextInt();
-            if (makerCombinationAnswer4 >= 6) {
-                makerCombinationAnswer4 = 6;
-            } else {
-                if (makerCombinationAnswer4 <= 1) {
-                    makerCombinationAnswer4 = 1;
-                }
-            }
-            System.out.println("Are you certain?");
-            System.out.println("1. Yes   2. No");
-            verify = input.nextInt();
-            if (verify == 1) {
-            } else {
-                if (verify == 2) {
-                } else {
-                }
-            }
-        } while (verify == 2);
-        System.out.println(makerCombinationAnswer1 + makerCombinationAnswer2 + makerCombinationAnswer3 + makerCombinationAnswer4);
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        int nextround;
-        int round;
+		do {
+			System.out.println("Create combination");
+			for (int i = 0; i < makerCombinationAnswer.length; i++) {
+				int in = input.nextInt();
 
-        round = 0;
-        do {
-            System.out.println("Breaker's turn");
-            System.out.println("Red = 1");
-            System.out.println("Blue = 2");
-            System.out.println("Yellow = 3");
-            System.out.println("Purple = 4");
-            System.out.println("Green = 5");
-            System.out.println("Orange = 6");
-            System.out.println("Place a colour you want by pressing the number");
-            System.out.println("Try to guess the Maker's combination");
-            int breakerRow1Space1;
-            int breakerRow1Space2;
-            int breakerRow1Space3;
-            int breakerRow1Space4;
+				if (in > 6) {
+					in = 6;
+				} if (in < 1) {
+					in = 1;
+				}
+				makerCombinationAnswer[i] = in;
+			}
+			System.out.println("" + makerCombinationAnswer[0] + makerCombinationAnswer[1] + makerCombinationAnswer[2] + makerCombinationAnswer[3]);
+			System.out.println("Are you certain?");
+			System.out.println("1. Yes   2. No");
+			verify = input.nextInt();
+			if (verify == 1) {
+			} else {
+				if (verify == 2) {
+				} else {
+				}
+			}
+		} while (verify == 2);
+		
+		System.out.println(" ");
+		System.out.println("Breaker's turn");
+		System.out.println("Red = 1. "
+				+ "Blue = 2. "
+				+ "Yellow = 3. "
+				+ "Purple = 4. "
+				+ "Green = 5. "
+				+ "Orange = 6. ");
 
-            breakerRow1Space1 = input.nextInt();
-            breakerRow1Space2 = input.nextInt();
-            breakerRow1Space3 = input.nextInt();
-            breakerRow1Space4 = input.nextInt();
-            int makerRow1Space1;
-            int makerRow1Space2;
-            int makerRow1Space3;
-            int makerRow1Space4;
+		System.out.println("Place a colour you want by pressing the number");
+		System.out.println("Try to guess the Maker's combination");
+		
+		
+		int[] breakerSpaces = new int[4];
+		int round = 0;
+		int roundOpposite = 10;
+		
+		do {
+	
+			for (int i = 0; i < breakerSpaces.length; i++) {
+				int in = input.nextInt();
+			
+				if (in >= 6) {
+					in = 6;
+				} else if (in <= 1) {
+					in = 1;
+				} 
+				breakerSpaces[i] = in;
+			}
+			System.out.println("" + breakerSpaces[0] + breakerSpaces[1] + breakerSpaces[2] + breakerSpaces[3]);
+			verify = breakerSpaces[0];
+			
+			// hier
+			if (verify == makerCombinationAnswer[0]){
+				breakerSpaces[0] = white;
+			
+			}
+			else breakerSpaces[0] = black;
+		
+			verify = breakerSpaces[1];
+			if (verify == makerCombinationAnswer[1]){
+				breakerSpaces[1] = white;
+			
+			}
+			else breakerSpaces[1] = black;
+			
+			verify = breakerSpaces[2];
+			if (verify == makerCombinationAnswer[2]){
+				breakerSpaces[2] = white;
+			
+			}
+			else breakerSpaces[2] = black;
+			
+			verify = breakerSpaces[3];
+			if (verify == makerCombinationAnswer[3]){
+				breakerSpaces[3] = white;
+			
+			}
+			else breakerSpaces[3] = black;
+			// tot hier
+			System.out.println("8 is white and 7 is black");
+			System.out.println("if a space is white then you guessed right, if it's black then it's wrong.");
+		    System.out.println("" + breakerSpaces[0] + breakerSpaces[1] + breakerSpaces[2] + breakerSpaces[3]);
+		    
+		    if (breakerSpaces[0] == white && breakerSpaces[1] == white && breakerSpaces[2] == white && breakerSpaces[3] == white) {
+				System.out.println("Breaker wins.");
+			}
+			
+			else round ++;
+		    roundOpposite --;
+		    System.out.println("Round " + round);
+		    System.out.println("You have " + roundOpposite + " tries left");
+		    
+		} while (round < 10);
+		    
+		    
+		    
+		
+		
 
-            if (breakerRow1Space1 >= 6) {
-                breakerRow1Space1 = 6;
-                System.out.println(breakerRow1Space1);
-            } else {
-                if (breakerRow1Space1 <= 1) {
-                    breakerRow1Space1 = 1;
-                }
-                System.out.println(breakerRow1Space1);
-            }
-            if (breakerRow1Space2 >= 6) {
-                breakerRow1Space2 = 6;
-                System.out.println(breakerRow1Space2);
-            } else {
-                if (breakerRow1Space2 <= 1) {
-                    breakerRow1Space2 = 1;
-                }
-                System.out.println(breakerRow1Space2);
-            }
-            if (breakerRow1Space3 >= 6) {
-                breakerRow1Space3 = 6;
-                System.out.println(breakerRow1Space3);
-            } else {
-                if (breakerRow1Space3 <= 1) {
-                    breakerRow1Space3 = 1;
-                }
-                System.out.println(breakerRow1Space3);
-            }
-            if (breakerRow1Space4 >= 6) {
-                breakerRow1Space4 = 6;
-                System.out.println(breakerRow1Space4);
-            } else {
-                if (breakerRow1Space4 <= 1) {
-                    breakerRow1Space4 = 1;
-                }
-                System.out.println(breakerRow1Space4);
-            }
-            if (breakerRow1Space1 == makerCombinationAnswer1) {
-                makerRow1Space1 = white;
-            } else {
-                makerRow1Space1 = black;
-            }
-            if (breakerRow1Space2 == makerCombinationAnswer2) {
-                makerRow1Space2 = white;
-            } else {
-                makerRow1Space2 = black;
-            }
-            if (breakerRow1Space3 == makerCombinationAnswer3) {
-                makerRow1Space3 = white;
-            } else {
-                makerRow1Space3 = black;
-            }
-            if (breakerRow1Space4 == makerCombinationAnswer4) {
-                makerRow1Space4 = white;
-            } else {
-                makerRow1Space4 = black;
-            }
-            System.out.println("8 is white and 7 is black");
-            System.out.println("if a space is white then you guessed right, if it's black then you guessed wrong");
-            System.out.println(makerRow1Space1 + makerRow1Space2 + makerRow1Space3 + makerRow1Space4);
-            if (makerRow1Space1 == white && makerRow1Space2 == white && makerRow1Space3 == white && makerRow1Space4 == white) {
-                System.out.println("Breaker wins");
-                nextround = 0;
-            } else {
-                System.out.println("Next round");
-                nextround = 1;
-            }
-            round = round + 1;
-        } while (nextround == 1 && round <= 10);
-        System.out.println("Out of turns, Maker wins");
-    }
+	}
+
 }
+
+//        System.out.println(makerCombinationAnswer1 + makerCombinationAnswer2 + makerCombinationAnswer3 + makerCombinationAnswer4);
+//        System.out.println("");
+//        System.out.println("");
+//        System.out.println("");
+//        int nextround;
+//        int round;
+//
+//        round = 0;
+//        do {
+//            System.out.println("Breaker's turn");
+//            System.out.println("Red = 1");
+//            System.out.println("Blue = 2");
+//            System.out.println("Yellow = 3");
+//            System.out.println("Purple = 4");
+//            System.out.println("Green = 5");
+//            System.out.println("Orange = 6");
+//            System.out.println("Place a colour you want by pressing the number");
+//            System.out.println("Try to guess the Maker's combination");
+//            int breakerRow1Space1;
+//            int breakerRow1Space2;
+//            int breakerRow1Space3;
+//            int breakerRow1Space4;
+//
+//            breakerRow1Space1 = input.nextInt();
+//            breakerRow1Space2 = input.nextInt();
+//            breakerRow1Space3 = input.nextInt();
+//            breakerRow1Space4 = input.nextInt();
+//            int makerRow1Space1;
+//            int makerRow1Space2;
+//            int makerRow1Space3;
+//            int makerRow1Space4;
+//
+//            if (breakerRow1Space1 >= 6) {
+//                breakerRow1Space1 = 6;
+//                System.out.println(breakerRow1Space1);
+//            } else {
+//                if (breakerRow1Space1 <= 1) {
+//                    breakerRow1Space1 = 1;
+//                }
+//                System.out.println(breakerRow1Space1);
+//            }
+//            if (breakerRow1Space2 >= 6) {
+//                breakerRow1Space2 = 6;
+//                System.out.println(breakerRow1Space2);
+//            } else {
+//                if (breakerRow1Space2 <= 1) {
+//                    breakerRow1Space2 = 1;
+//                }
+//                System.out.println(breakerRow1Space2);
+//            }
+//            if (breakerRow1Space3 >= 6) {
+//                breakerRow1Space3 = 6;
+//                System.out.println(breakerRow1Space3);
+//            } else {
+//                if (breakerRow1Space3 <= 1) {
+//                    breakerRow1Space3 = 1;
+//                }
+//                System.out.println(breakerRow1Space3);
+//            }
+//            if (breakerRow1Space4 >= 6) {
+//                breakerRow1Space4 = 6;
+//                System.out.println(breakerRow1Space4);
+//            } else {
+//                if (breakerRow1Space4 <= 1) {
+//                    breakerRow1Space4 = 1;
+//                }
+//                System.out.println(breakerRow1Space4);
+//            }
+//            if (breakerRow1Space1 == makerCombinationAnswer1) {
+//                makerRow1Space1 = white;
+//            } else {
+//                makerRow1Space1 = black;
+//            }
+//            if (breakerRow1Space2 == makerCombinationAnswer2) {
+//                makerRow1Space2 = white;
+//            } else {
+//                makerRow1Space2 = black;
+//            }
+//            if (breakerRow1Space3 == makerCombinationAnswer3) {
+//                makerRow1Space3 = white;
+//            } else {
+//                makerRow1Space3 = black;
+//            }
+//            if (breakerRow1Space4 == makerCombinationAnswer4) {
+//                makerRow1Space4 = white;
+//            } else {
+//                makerRow1Space4 = black;
+//            }
+//            System.out.println("8 is white and 7 is black");
+//            System.out.println("if a space is white then you guessed right, if it's black then you guessed wrong");
+//            System.out.println(makerRow1Space1 + makerRow1Space2 + makerRow1Space3 + makerRow1Space4);
+//            if (makerRow1Space1 == white && makerRow1Space2 == white && makerRow1Space3 == white && makerRow1Space4 == white) {
+//                System.out.println("Breaker wins");
+//                nextround = 0;
+//            } else {
+//                System.out.println("Next round");
+//                nextround = 1;
+//            }
+//            round = round + 1;
+//        } while (nextround == 1 && round <= 10);
+//        System.out.println("Out of turns, Maker wins");
