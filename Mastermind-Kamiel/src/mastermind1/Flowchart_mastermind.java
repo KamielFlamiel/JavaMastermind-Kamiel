5cbde295bef574d355fc0f6becf3c8472a637d1d
KamielFlamiel <justinmeijer2007@outlook.com>
2024-10-10 15:30:20 +0200

    nightmare

--- Mastermind-Kamiel/src/mastermind1/Flowchart_mastermind.java
+++ Mastermind-Kamiel/src/mastermind1/Flowchart_mastermind.java
@@ -1,6 +1,7 @@
 package mastermind1;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Flowchart_mastermind {
 	private static Scanner input = new Scanner(System.in);
@@ -10,44 +11,26 @@ public class Flowchart_mastermind {
 		int white = 8;
 		int empty = 0;
 
-		System.out.println("Maker, Create combination.");
-		System.out.println("Red = 1");
-		System.out.println("Blue = 2");
-		System.out.println("Yellow = 3");
-		System.out.println("Purple = 4");
-		System.out.println("Green = 5");
-		System.out.println("Orange = 6");
 		int[] makerCombinationAnswer = new int[4];
 		int verify;
+		Random rand = new Random();
 
-		do {
-			System.out.println("Create combination");
-			for (int i = 0; i < makerCombinationAnswer.length; i++) {
-				int in = input.nextInt();
+		for (int i = 0; i < makerCombinationAnswer.length; i++) {
+			int in = rand.nextInt(6) + 1;
 
-				if (in > 6) {
-					in = 6;
-				}
-				if (in < 1) {
-					in = 1;
-				}
-				makerCombinationAnswer[i] = in;
+			if (in > 6) {
+				in = 6;
 			}
-			System.out.println("" + makerCombinationAnswer[0] + makerCombinationAnswer[1] + makerCombinationAnswer[2]
-					+ makerCombinationAnswer[3]);
-			System.out.println("Are you certain?");
-			System.out.println("1. Yes   2. No");
-			verify = input.nextInt();
-			if (verify == 1) {
-			} else {
-				if (verify == 2) {
-				} else {
-				}
+			if (in < 1) {
+				in = 1;
 			}
-		} while (verify == 2);
+			makerCombinationAnswer[i] = in;
+		}
+		System.out.println("" + makerCombinationAnswer[0] + makerCombinationAnswer[1] + makerCombinationAnswer[2]
+				+ makerCombinationAnswer[3]);
 
 		System.out.println(" ");
-		System.out.println("Breaker's turn");
+		System.out.println("The maker has made a combination");
 		System.out
 				.println("Red = 1. " + "Blue = 2. " + "Yellow = 3. " + "Purple = 4. " + "Green = 5. " + "Orange = 6. ");
 
@@ -57,6 +40,7 @@ public class Flowchart_mastermind {
 		int[] breakerSpaces = new int[4];
 		int round = 0;
 		int roundOpposite = 10;
+		int randomSpace;
 
 		do {
 
@@ -72,62 +56,93 @@ public class Flowchart_mastermind {
 			}
 			System.out.println("" + breakerSpaces[0] + breakerSpaces[1] + breakerSpaces[2] + breakerSpaces[3]);
 
-			for (int check = 0; check < breakerSpaces.length; check++) {
+			int[] checkSpaces = { -1, -1, -1, -1 };
+			// for (int check = 0; check < breakerSpaces.length; check++) {
 
-				verify = breakerSpaces[check];
+			verify = breakerSpaces[0];
 
-				// 1e
-				if (verify == makerCombinationAnswer[0]) {
-					breakerSpaces[0] = white; // als makercombination[0] gelijk is aan verify - juiste kleur, juiste
-												// plek
+			// 1e
+			int randomnum = rand.nextInt(4);
+			int check = breakerSpaces[randomnum];
+
+			System.out.println("lol");
+			while (check != -1) {
+				randomnum = rand.nextInt(4);
+				check = checkSpaces[randomnum];
+			}
+
+			if (verify == makerCombinationAnswer[0]) {
+				checkSpaces[0] = white; // als makercombination[0] gelijk is aan verify - juiste kleur, juiste
+										// plek
+			} else {
+				if (verify != makerCombinationAnswer[1] || verify != makerCombinationAnswer[2]
+						|| verify != makerCombinationAnswer[3]) {
+
+					checkSpaces[randomnum] = black;
 				} else {
-					if (verify == makerCombinationAnswer[1] 
-							|| verify == makerCombinationAnswer[2]
-							|| verify == makerCombinationAnswer[3])
-						breakerSpaces[0] = black;
-				}
-				// 2e
-				if (verify == makerCombinationAnswer[1]) {
-					breakerSpaces[1] = white;
-					
-				} else {
-					if (verify == makerCombinationAnswer[1] 
-							|| verify != makerCombinationAnswer[2]
-							|| verify != makerCombinationAnswer[3])
-						breakerSpaces[1] = black;
-				}
-				// 3e
-				if (verify == makerCombinationAnswer[2]) {
-					breakerSpaces[2] = white;
-					
-				} else {
-					if (verify == makerCombinationAnswer[2] 
-							|| verify != makerCombinationAnswer[2]
-							|| verify != makerCombinationAnswer[3])
-						breakerSpaces[2] = black;
-				}
-				// 4e
-				if (verify == makerCombinationAnswer[3]) {
-					breakerSpaces[3] = white;
-				} else {
-					if (verify == makerCombinationAnswer[3] 
-							|| verify != makerCombinationAnswer[2]
-							|| verify != makerCombinationAnswer[3])
-						breakerSpaces[3] = black;
+					checkSpaces[0] = empty;
 				}
 			}
+			verify = breakerSpaces[1];
+
+			// 2e
+			if (verify == makerCombinationAnswer[1]) {
+				checkSpaces[1] = white;
+
+			} else {
+				if (verify != makerCombinationAnswer[1] || verify != makerCombinationAnswer[2]
+						|| verify != makerCombinationAnswer[3])
+					checkSpaces[randomnum] = black;
+
+				else {
+					checkSpaces[1] = empty;
+				}
+
+			}
+
+			verify = breakerSpaces[2];
+			// 3e
+			if (verify == makerCombinationAnswer[2]) {
+				checkSpaces[2] = white;
+
+			} else {
+				if (verify != makerCombinationAnswer[2] || verify != makerCombinationAnswer[2]
+						|| verify != makerCombinationAnswer[3])
+					checkSpaces[randomnum] = black;
+
+				else {
+					checkSpaces[2] = empty;
+				}
+			}
+
+			verify = breakerSpaces[3];
+			// 4e
+			if (verify == makerCombinationAnswer[3]) {
+				checkSpaces[3] = white;
+			} else {
+				if (verify != makerCombinationAnswer[3] || verify != makerCombinationAnswer[2]
+						|| verify != makerCombinationAnswer[3])
+					checkSpaces[randomnum] = black;
+
+				else {
+					checkSpaces[3] = empty;
+				}
+			}
+			// }
 
 			System.out.println("8 is white and 7 is black");
 			System.out.println("if a space is white then you guessed right, if it's black then it's wrong.");
-			System.out.println("" + breakerSpaces[0] + breakerSpaces[1] + breakerSpaces[2] + breakerSpaces[3]);
+			System.out.println("" + checkSpaces[0] + checkSpaces[1] + checkSpaces[2] + checkSpaces[3]);
 
-			if (breakerSpaces[0] == white && breakerSpaces[1] == white && breakerSpaces[2] == white
-					&& breakerSpaces[3] == white) {
+			if (checkSpaces[0] == white && checkSpaces[1] == white && checkSpaces[2] == white
+					&& checkSpaces[3] == white) {
 				System.out.println("Breaker wins.");
+				break;
 			}
 
-			else
+			else {
 				round++;
+			}
 			roundOpposite--;
 			System.out.println("Round " + round);
 			System.out.println("You have " + roundOpposite + " tries left");
